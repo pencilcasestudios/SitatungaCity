c4bd24870fc8e2c8a7cee8109251b491aee92d36
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2015-03-27 10:44:10 +0200

    Updated the Housing Project model.

--- app/models/housing_project.rb
+++ app/models/housing_project.rb
@@ -1,15 +1,6 @@
 class HousingProject < ActiveRecord::Base
   has_many :plots
 
-  attr_accessible :cover_image
-  attr_accessible :gps_coordinates
-  attr_accessible :name
-  attr_accessible :plot_layout_image
-  attr_accessible :plot_layout_image_dimensions
-  attr_accessible :state
-  attr_accessible :subdomain, uniqueness: true
-  attr_accessible :summary
-
   serialize :gps_coordinates
   serialize :plot_layout_image_dimensions
 
