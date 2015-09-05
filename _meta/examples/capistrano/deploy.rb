3b2983a743d267651df4f5110bbddc5c92aa37fc
Silumesii Maboshe <silumesii@pencilcasestudios.com>
2015-09-05 21:51:12 +0200

    Example deployment configuration tweak.

--- _meta/examples/capistrano/deploy.rb
+++ _meta/examples/capistrano/deploy.rb
@@ -22,66 +22,66 @@ set :asset_vault_path, DEPLOYMENT_CONFIG["asset_vault_path"]
 
 
 set :linked_dirs, %w{
-  app/assets/images
-  app/assets/pdfs
-  bin
-  log
-  public/system
-  tmp/cache
-  tmp/pids
-  tmp/sockets
-  vendor/bundle
+	app/assets/images
+	app/assets/pdfs
+	bin
+	log
+	public/system
+	tmp/cache
+	tmp/pids
+	tmp/sockets
+	vendor/bundle
 }
 
 set :linked_files, %w{
-  .env
-  config/config.yml
-  config/database.yml
+	.env
+	config/config.yml
+	config/database.yml
 }
 
 
 
 
 namespace :deploy do
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+	after :restart, :clear_cache do
+		on roles(:web), in: :groups, limit: 3, wait: 10 do
+			# Here we can do anything such as:
+			# within release_path do
+			#   execute :rake, 'cache:clear'
+			# end
+		end
+	end
 
-  after :publishing, :restart
+	after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, "cache:clear"
-      # end
-    end
-  end
+	after :restart, :clear_cache do
+		on roles(:web), in: :groups, limit: 3, wait: 10 do
+			# Here we can do anything such as:
+			# within release_path do
+			#   execute :rake, "cache:clear"
+			# end
+		end
+	end
 
-  desc "Set up externalised asset folders."
-  task :setup_asset_vault do
-    on roles(:all) do
-      puts "Create asset folders in #{fetch(:asset_vault_path)}/#{fetch(:application)}"
+	desc "Set up externalised asset folders."
+	task :setup_asset_vault do
+		on roles(:all) do
+			puts "Create asset folders in #{fetch(:asset_vault_path)}/#{fetch(:application)}"
 
-      execute "mkdir -p #{fetch(:asset_vault_path)}/#{fetch(:application)}/Images/"
-      execute "mkdir -p #{fetch(:asset_vault_path)}/#{fetch(:application)}/PDFs/"
+			execute "mkdir -p #{fetch(:asset_vault_path)}/#{fetch(:application)}/Images/"
+			execute "mkdir -p #{fetch(:asset_vault_path)}/#{fetch(:application)}/PDFs/"
 
-      execute "chown -R #{fetch(:deploy_user)}:#{fetch(:deploy_user)} #{fetch(:asset_vault_path)}"
-    end
-  end
+			execute "chown -R #{fetch(:deploy_user)}:#{fetch(:deploy_user)} #{fetch(:asset_vault_path)}"
+		end
+	end
 
-  # Ref: https://github.com/rvm/rvm1-capistrano3#security
-  desc "Update the RVM key"
-  task :update_rvm_key do
-    on roles(:all) do
-      execute :gpg, "--keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
-    end
-  end
+	# Ref: https://github.com/rvm/rvm1-capistrano3#security
+	desc "Update the RVM key"
+	task :update_rvm_key do
+		on roles(:all) do
+			execute :gpg, "--keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3"
+		end
+	end
 end
 
 
@@ -95,63 +95,63 @@ end
 # Use Capistrano and rake to take care of helpful database tasks
 # Ref: https://github.com/dei79/capistrano-rails-collection/blob/master/lib/capistrano/tasks/db.rake
 namespace :db do
-  desc "Drop the database to an empty state"
-  task :drop do
-    on roles(:all) do
-      within release_path do
-        execute :rake, "db:drop RAILS_ENV=#{fetch(:rails_env)}"
-      end
-    end
-  end
+	desc "Drop the database to an empty state"
+	task :drop do
+		on roles(:all) do
+			within release_path do
+				execute :rake, "db:drop RAILS_ENV=#{fetch(:rails_env)}"
+			end
+		end
+	end
 
 
-  desc "Reset the database to an empty state"
-  task :reset do
-    on roles(:all) do
-      within release_path do
-        execute :rake, "db:reset RAILS_ENV=#{fetch(:rails_env)}"
-      end
-    end
-  end
+	desc "Reset the database to an empty state"
+	task :reset do
+		on roles(:all) do
+			within release_path do
+				execute :rake, "db:reset RAILS_ENV=#{fetch(:rails_env)}"
+			end
+		end
+	end
 
 
-  desc "Create the database, load the schema, and initialize with the seed data (use db:reset to also drop the db first)"
-  task :setup do
-    on roles(:all) do
-      within release_path do
-        execute :rake, "db:setup RAILS_ENV=#{fetch(:rails_env)}"
-      end
-    end
-  end
+	desc "Create the database, load the schema, and initialize with the seed data (use db:reset to also drop the db first)"
+	task :setup do
+		on roles(:all) do
+			within release_path do
+				execute :rake, "db:setup RAILS_ENV=#{fetch(:rails_env)}"
+			end
+		end
+	end
 
 
-  desc "Load the seed data from db/seeds.rb"
-  task :seed do
-    on roles(:all) do
-      within release_path do
-        execute :rake, "db:seed RAILS_ENV=#{fetch(:rails_env)}"
-      end
-    end
-  end
+	desc "Load the seed data from db/seeds.rb"
+	task :seed do
+		on roles(:all) do
+			within release_path do
+				execute :rake, "db:seed RAILS_ENV=#{fetch(:rails_env)}"
+			end
+		end
+	end
 
 
-  desc "Migrate the database"
-  task :migrate do
-    on roles(:all) do
-      within release_path do
-        execute :rake, "db:migrate RAILS_ENV=#{fetch(:rails_env)}"
-      end
-    end
-  end
+	desc "Migrate the database"
+	task :migrate do
+		on roles(:all) do
+			within release_path do
+				execute :rake, "db:migrate RAILS_ENV=#{fetch(:rails_env)}"
+			end
+		end
+	end
 
-  desc "Rolls the schema back to the previous version"
-  task :rollback do
-    on roles(:all) do
-      within release_path do
-        execute :rake, "db:rollback RAILS_ENV=#{fetch(:rails_env)}"
-      end
-    end
-  end
+	desc "Rolls the schema back to the previous version"
+	task :rollback do
+		on roles(:all) do
+			within release_path do
+				execute :rake, "db:rollback RAILS_ENV=#{fetch(:rails_env)}"
+			end
+		end
+	end
 end
 
 
